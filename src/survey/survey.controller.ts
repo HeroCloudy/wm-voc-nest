e9a45605f89aba19c61d839e4a26f90a65880d67
youyacoder <yygnb@126.com>
2025-10-20 16:56:34 +0800

    测试query\param传参

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('survey')
 export class SurveyController {
   @Get()
-  findAll() {
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    console.log(keyword, page, pageSize);
     return {
       list: [1, 2, 3],
       count: 100,
     };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id, test: 'test' };
   }
 }
