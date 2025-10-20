d6d0f68b8cc56ad8810095504f89c40157051f2c
youyacoder <yygnb@126.com>
2025-10-20 17:58:22 +0800

    feat: 使用 typeorm 连接 mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SurveyModule } from './survey/survey.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SurveyModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Mysql.123',
+      database: 'voc',
+      synchronize: true,
+    }),
+    SurveyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
